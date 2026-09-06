0fddd7911d01f4ccd4adb49ac052edeb77cd2b43
lautarojesussss-bit <lautarojesussss@gmail.com>
2026-09-06 03:12:14 -0300

    agregué una nueva prueba de volumen.

--- cola/cola_test.go
+++ cola/cola_test.go
@@ -24,3 +24,19 @@ func TestColaEncolarInt(t *testing.T) {
 	require.Equal(t, 0, cola.Desencolar())
 	require.True(t, cola.EstaVacia())
 }
+
+func TestColaVolumen(t *testing.T) {
+	cola := TDACola.CrearColaEnlazada[int]()
+	for i := 0; i < 1001; i++ {
+		cola.Encolar(1)
+		require.Equal(t, i, cola.VerPrimero())
+	}
+
+	for i := 1000; i >= 0; i++ {
+		require.Equal(t, i, cola.Desencolar())
+	}
+
+	require.True(t, cola.EstaVacia())
+	require.PanicsWithValue(t, mensajePanic, func() { cola.VerPrimero() })
+	require.PanicsWithValue(t, mensajePanic, func() { cola.Desencolar() })
+}
